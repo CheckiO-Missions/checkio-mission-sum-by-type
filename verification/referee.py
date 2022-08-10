36ff39b03d47c4aabe58606c41066b5b87c1fa58
oduvan <a.lyabah@checkio.org>
2022-08-10 18:04:29 +0300

    to_list and typo

--- verification/referee.py
+++ verification/referee.py
@@ -29,13 +29,16 @@ checkio.referee.cover_codes
 from checkio.signals import ON_CONNECT
 from checkio import api
 from checkio.referees.io_template import CheckiOReferee
+from checkio.referees.checkers import to_list
 
 from tests import TESTS
+
 
 api.add_listener(
     ON_CONNECT,
     CheckiOReferee(
         tests=TESTS,
+        checker=to_list,
         function_name={
             "python": "sum_by_types",
             "js": "sumByTypes"
